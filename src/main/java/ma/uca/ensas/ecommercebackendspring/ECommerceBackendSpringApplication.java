8849399dbc3c8e85067be14c6e9521715ae35475
Imad Eddine <imadhajali66@gmail.com>
2022-03-20 11:49:03 +0100

    first commit

---
+++ src/main/java/ma/uca/ensas/ecommercebackendspring/ECommerceBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package ma.uca.ensas.ecommercebackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceBackendSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceBackendSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/uca/ensas/ecommercebackendspring/ECommerceBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.uca.ensas.ecommercebackendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackendSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
